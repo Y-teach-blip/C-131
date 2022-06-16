e4e42cb68204466c6d526cf2623d982d8d4de538
Yoshi Sharma <73175197+Y-teach-blip@users.noreply.github.com>
2022-06-16 11:30:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function desk()
+{
+    window.location = "desk.html";
+}
+
+function living_room()
+{
+    window.location = "living_room.html";
+}
+
+function fruit_basket()
+{
+    window.location = "fruit_basket.html";
+}
+
+function tv()
+{
+    window.location = "tv.html";
+}
+
+function bottle()
+{
+    window.location = "oottle.html";
+}
